fc29cf3a252db5d048f1bce35cd69c80d68e6e13
Cruziblex <dragex@yahoo.com>
2024-04-17 09:01:12 -0400

    Stop player movement out of bounds
    Added a script that will stop the player movement if crossing out of bounds

--- Assets/Scripts/PlayerColliderScript.cs
+++ Assets/Scripts/PlayerColliderScript.cs
@@ -3,13 +3,26 @@ using UnityEngine;
 
 public class PlayerColliderScript : MonoBehaviour
 {
+    private void Start()
+    {
+        PlayerMovement = gameObject.GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        float xPosition = gameObject.transform.position.x;
+        if (xPosition <= -10.5 || xPosition >= 10.5)
+        {
+            PlayerMovement.enabled = false;
+        }
+    }
     private PlayerMovement PlayerMovement;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Obstacle")
         {
-            PlayerMovement = gameObject.GetComponent<PlayerMovement>();
             PlayerMovement.enabled = false;
         }
     }
+
 }
